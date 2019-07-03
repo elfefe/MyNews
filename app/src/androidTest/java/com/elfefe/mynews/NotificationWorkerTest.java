cb0edfd17b04e627ce501492ecb2f7982621d083
Nino <anthony@delcey.fr>
2019-07-03 18:54:31 +0200

    Fix WorkManager tests

--- app/src/androidTest/java/com/elfefe/mynews/NotificationWorkerTest.java
+++ app/src/androidTest/java/com/elfefe/mynews/NotificationWorkerTest.java
@@ -1,5 +1,6 @@
 package com.elfefe.mynews;
 
+
 import android.content.Context;
 import android.util.Log;
 
@@ -8,7 +9,9 @@ import androidx.work.Configuration;
 import androidx.work.impl.utils.SynchronousExecutor;
 import androidx.test.runner.*;
 
+import androidx.work.testing.WorkManagerTestInitHelper;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
@@ -17,15 +20,19 @@ public class NotificationWorkerTest {
     public void setup() {
         Context context = InstrumentationRegistry.getTargetContext();
         Configuration config = new Configuration.Builder()
-                // Set log level to Log.DEBUG to
-                // make it easier to see why tests failed
-                .setMinimumLoggingLevel(Log.DEBUG)
-                // Use a SynchronousExecutor to make it easier to write tests
-                .setExecutor(new SynchronousExecutor())
-                .build();
+            // Set log level to Log.DEBUG to
+            // make it easier to see why tests failed
+            .setMinimumLoggingLevel(Log.DEBUG)
+            // Use a SynchronousExecutor to make it easier to write tests
+            .setExecutor(new SynchronousExecutor())
+            .build();
 
         // Initialize WorkManager for instrumentation tests.
-        WorkManagerTestInitHelper.initializeTestWorkManager(
-                context, config);
+        WorkManagerTestInitHelper.initializeTestWorkManager(context, config);
+    }
+
+    @Test
+    public void toto() {
+
     }
 }
